b47ad7fc42eb6254c11e4589bfe3c50258f40dd1
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-01-14 19:41:19 +0100

    done javescript

---
+++ script.js
@@ -0,0 +1,19 @@
+const portfolio = document.getElementById("portfolio");
+const portfolioBtn = document.getElementById("portfolio-btn");
+const skills = document.getElementById("skills");
+const skillsBtn = document.getElementById("skills-btn");
+
+portfolioBtn.addEventListener("click", (event) => {
+    skills.style.display = "none";
+    portfolio.style.display = "flex";
+    skillsBtn.classList.remove("active-btn");
+    portfolioBtn.classList.add("active-btn");
+});
+
+portfolioBtn.addEventListener("click", (event) => {
+    skills.style.display = "none";
+    portfolio.style.display = "flex";
+    skillsBtn.classList.remove("active-btn");
+    portfolioBtn.classList.add("active-btn");
+});
+

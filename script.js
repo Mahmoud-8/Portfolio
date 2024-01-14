a12a8ef1879179b8fcb216a5414415788c84fee5
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-01-14 20:06:35 +0100

    done javescript

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ portfolioBtn.addEventListener("click", (event) => {
     portfolioBtn.classList.add("active-btn");
 });
 
-portfolioBtn.addEventListener("click", (event) => {
-    skills.style.display = "none";
-    portfolio.style.display = "flex";
-    skillsBtn.classList.remove("active-btn");
-    portfolioBtn.classList.add("active-btn");
+skillsBtn.addEventListener("click", (event) => {
+    portfolio.style.display = "none";
+    skills.style.display = "flex";
+    portfolioBtn.classList.remove("active-btn");
+    skillsBtn.classList.add("active-btn");
 });
 
